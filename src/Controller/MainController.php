54d49ec644d3cd996cca44f611ef225ea135ed4a
riddikpunisheur <shevyshepard@oclock.io>
2020-06-26 14:22:59 +0200

    test1 home ok

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -9,12 +9,10 @@ use Symfony\Component\Routing\Annotation\Route;
 class MainController extends AbstractController
 {
     /**
-     * 
      * @Route("/", name="homepage")
      */
     public function homepage()
     {
-        
-        return $this->render('homepage.html.twig', ["date" => "26/06/2020"]);
+        return $this->render('homepage.html.twig');
     }
 }
